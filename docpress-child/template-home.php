12754d907d3f5545e2824aea3e5fe7ef4034e903
Caio Oliveira <caio.oliveira07@gmail.com>
2018-02-26 17:23:23 -0300

    Ícone Rubricas adicionado e link inserido nas ferramentas

--- docpress-child/template-home.php
+++ docpress-child/template-home.php
@@ -13,71 +13,88 @@
 					<h1> Mais acessados </h1>
 						<div class="categories">
 							<div class="row">
-								<div class="col-md-3 text-center box-categories">
-									<a href="comecando-com-o-blackboard" target="_blank" rel="noopener">
-									<img class="size-full wp-image-9 alignnone" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-casa.png" alt="" width="64" height="57" /><br/>
-									<p><b>Nunca entrou no Blackboard?</b></p>
-									<p>Aqui você pode conhecer um pouco de todos os recursos que ele oferece</p></a>
-								</div>
-								<div class="col-md-3 text-center box-categories">
-									<a href="testes-pesquisas-e-bancos-de-testes" target="_blank" rel="noopener">
-									<img class="alignnone size-full wp-image-20" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-teste.png" alt="" width="62" height="57" /><br/>
-									<p><b>Tem dúvidas sobre como aplicar testes?</b></p>
-									<p>Aqui você terá um passo a passo sobre aplicação de testes e banco de questões</p></a>
-								</div>
-								<div class="col-md-3 text-center box-categories">
-									<a href="exercicios" target="_blank" rel="noopener">
-									<img class="alignnone size-full wp-image-21" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-exercicios.png" alt="" width="64" height="59" /><br/>
-									<p><b>Quer receber todas as atividades em um único lugar?</b></p>
-									<p> A ferramenta de exercícios pode te ajudar nisso</p></a>
-								</div>
-								<div class="col-md-3 text-center box-categories">
-									<a href="centro-de-notas" target="_blank" rel="noopener">
-									<img class="alignnone size-full wp-image-21" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-centro-notas.png" alt="" width="64" height="64" /><br/>
-									<p><b>Quer organizar o lançamento de notas?</b></p>
-									<p>Conheça mais sobre a ferramenta Centro de Notas</p></a>
-								</div>
+								<a href="comecando-com-o-blackboard" target="_blank" rel="noopener">
+									<div class="col-md-3 text-center box-categories">
+										<img class="size-full wp-image-9 alignnone" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-casa.png" alt="" width="64" height="57" /><br/>
+										<p><b>Nunca entrou no Blackboard?</b></p>
+										<p>Aqui você pode conhecer um pouco de todos os recursos que ele oferece</p>
+									</div>
+								</a>
+								<a href="testes-pesquisas-e-bancos-de-testes" target="_blank" rel="noopener">
+									<div class="col-md-3 text-center box-categories">
+										<img class="alignnone size-full wp-image-20" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-teste.png" alt="" width="62" height="57" /><br/>
+										<p><b>Tem dúvidas sobre como aplicar testes?</b></p>
+										<p>Aqui você terá um passo a passo sobre aplicação de testes e banco de questões</p>
+									</div>
+								</a>
+								<a href="exercicios" target="_blank" rel="noopener">
+									<div class="col-md-3 text-center box-categories">
+										<img class="alignnone size-full wp-image-21" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-exercicios.png" alt="" width="64" height="59" /><br/>
+										<p><b>Quer receber todas as atividades em um único lugar?</b></p>
+										<p> A ferramenta de exercícios pode te ajudar nisso</p>
+									</div>
+								</a>
+								<a href="centro-de-notas" target="_blank" rel="noopener">
+									<div class="col-md-3 text-center box-categories">
+										<img class="alignnone size-full wp-image-21" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-centro-notas.png" alt="" width="64" height="64" /><br/>
+										<p><b>Quer organizar o lançamento de notas?</b></p>
+										<p>Conheça mais sobre a ferramenta Centro de Notas</p>
+									</div>
+								</a>
 							</div>
 						</div>
 
 					<h1> Ferramentas </h1>
 					<div class="categories">
 						<div class="row">
-							<div class="col-md-3 text-center box-categories">
-								<a href="comecando-com-o-blackboard/#criar_aviso" target="_blank" rel="noopener">
-								<img class="size-full wp-image-9 alignnone" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-avisos.png" alt="" /><br/>
-								<p>Avisos</p></a>
-							</div>
-							<div class="col-md-3 text-center box-categories">
-								<a href="exercicios" target="_blank" rel="noopener">
-								<img class="alignnone size-full wp-image-20" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-exercicios.png" alt="" /><br/>
-								<p>Entrega de exercício</p></a>
-							</div>
-							<div class="col-md-3 text-center box-categories">
-								<a href="testes-pesquisas-e-bancos-de-testes" target="_blank" rel="noopener">
-								<img class="alignnone size-full wp-image-21" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-teste.png" alt=""/><br/>
-								<p>Testes, Pesquisas e Banco de Testes</p></a>
-							</div>
-							<div class="col-md-3 text-center box-categories">
-								<a href="centro-de-notas" target="_blank" rel="noopener">
-								<img class="alignnone size-full wp-image-21" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-centro-notas.png" alt="" /><br/>
-								<p>Centro de Notas</p></a>
-							</div>
-							<div class="col-md-3 text-center box-categories">
-								<a href="grupos" target="_blank" rel="noopener">
-								<img class="size-full wp-image-9 alignnone" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-group.png" alt="" /><br/>
-								<p>Grupos</p></a>
-							</div>
-							<div class="col-md-3 text-center box-categories">
-								<a href="collaborate-ultra" target="_blank" rel="noopener">
-								<img class="alignnone size-full wp-image-20" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-collaborate-1.png" alt=""/><br/>
-								<p>Collaborate</p></a>
-							</div>
-							<div class="col-md-3 text-center box-categories">
-								<a href="forum-de-discussao" target="_blank" rel="noopener">
-								<img class="alignnone size-full wp-image-21" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-forum.png" alt="" /><br/>
-								<p>Fórum de discussão</p></a>
-							</div>
+							<a href="comecando-com-o-blackboard/#criar_aviso" target="_blank" rel="noopener">
+								<div class="col-md-3 text-center box-categories">
+									<img class="size-full wp-image-9 alignnone" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-avisos.png" alt="" /><br/>
+									<p>Avisos</p>
+								</div>
+							</a>
+							<a href="exercicios" target="_blank" rel="noopener">
+								<div class="col-md-3 text-center box-categories">
+									<img class="alignnone size-full wp-image-20" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-exercicios.png" alt="" /><br/>
+									<p>Entrega de exercício</p>
+								</div>
+							</a>
+							<a href="testes-pesquisas-e-bancos-de-testes" target="_blank" rel="noopener">
+								<div class="col-md-3 text-center box-categories">
+									<img class="alignnone size-full wp-image-21" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-teste.png" alt=""/><br/>
+									<p>Testes, Pesquisas e Banco de Testes</p>
+								</div>
+							</a>
+							<a href="centro-de-notas" target="_blank" rel="noopener">
+								<div class="col-md-3 text-center box-categories">
+									<img class="alignnone size-full wp-image-21" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-centro-notas.png" alt="" /><br/>
+									<p>Centro de Notas</p>
+								</div>
+							</a>
+							<a href="grupos" target="_blank" rel="noopener">
+								<div class="col-md-3 text-center box-categories">
+									<img class="size-full wp-image-9 alignnone" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-group.png" alt="" /><br/>
+									<p>Grupos</p>
+								</div>
+							</a>
+							<a href="collaborate-ultra" target="_blank" rel="noopener">
+								<div class="col-md-3 text-center box-categories">	
+									<img class="alignnone size-full wp-image-20" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-collaborate-1.png" alt=""/><br/>
+									<p>Collaborate</p>
+								</div>
+							</a>
+							<a href="forum-de-discussao" target="_blank" rel="noopener">
+								<div class="col-md-3 text-center box-categories">
+									<img class="alignnone size-full wp-image-21" src="https://kb.espm.br/wp-content/uploads/2018/01/icon-forum.png" alt="" /><br/>
+									<p>Fórum de discussão</p>
+								</div>
+							</a>
+							<a href="rubricas" target="_blank" rel="noopener">
+								<div class="col-md-3 text-center box-categories">
+									<img class="alignnone size-full wp-image-21" src="https://kb.espm.br/wp-content/uploads/2018/02/icon-rubricas.png" alt="" /><br/>
+									<p>Rubricas</p>
+								</div>
+							</a>
 						</div>
 					</div>
 					<!--<h1> Casos práticos </h1>
